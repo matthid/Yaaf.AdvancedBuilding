774750d8344a4eacca87b7cce566fff0ac997761
Matthias Dittrich <matthi.d@googlemail.com>
2015-04-06 19:13:09 +0200

    fix assembly info.

--- src/SharedAssemblyInfo.fs
+++ src/SharedAssemblyInfo.fs
@@ -4,7 +4,6 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Yaaf.AdvancedBuilding")>]
 [<assembly: AssemblyProductAttribute("Yaaf.AdvancedBuilding")>]
 [<assembly: AssemblyCopyrightAttribute("Yaaf.AdvancedBuilding Copyright © Matthias Dittrich 2015")>]
-<<<<<<< Updated upstream
 [<assembly: AssemblyVersionAttribute("0.5.4")>]
 [<assembly: AssemblyFileVersionAttribute("0.5.4")>]
 [<assembly: AssemblyInformationalVersionAttribute("0.5.4")>]
@@ -12,12 +11,3 @@ do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] Version = "0.5.4"
-=======
-[<assembly: AssemblyVersionAttribute("0.4.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.4.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.4.0")>]
-do ()
-
-module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.4.0"
->>>>>>> Stashed changes
