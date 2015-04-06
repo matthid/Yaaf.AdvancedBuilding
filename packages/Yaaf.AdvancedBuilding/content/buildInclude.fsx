20301119944c1f796f2d59619c69faa6c9320d8e
Matthias Dittrich <matthi.d@googlemail.com>
2015-04-06 20:56:39 +0200

    we can create pdb only on windows therefore include mdb in the nuget package (to be able to create the package on non-windows platforms.

--- packages/Yaaf.AdvancedBuilding/content/buildInclude.fsx
+++ packages/Yaaf.AdvancedBuilding/content/buildInclude.fsx
@@ -56,15 +56,17 @@ if config.UseNuget then
 
 let createMissingSymbolFiles assembly =
   try
-    match File.Exists (Path.ChangeExtension(assembly, "pdb")), File.Exists (Path.ChangeExtension (assembly, "mdb")) with
+    match File.Exists (Path.ChangeExtension(assembly, "pdb")), File.Exists (assembly + ".mdb") with
     | true, false ->
       // create mdb
-      trace (sprintf "Creating mdb for %s" assembly)
+      traceVerbose (sprintf "Creating mdb for %s" assembly)
       DebugSymbolHelper.writeMdbFromPdb assembly
-    | false, true ->
+    | false, true when not isLinux ->
       // create pdb
-      trace (sprintf "Creating pdb for %s" assembly)
+      traceVerbose (sprintf "Creating pdb for %s" assembly)
       DebugSymbolHelper.writePdbFromMdb assembly
+    | false, true ->
+      traceVerbose (sprintf "Cannot create pdb for %s because we are not on windows :(" assembly) 
     | _, _ -> 
       // either no debug symbols available or already both.
       ()
