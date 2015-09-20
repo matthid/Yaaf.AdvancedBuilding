56ed8b0a569427b901fdf3f2d7b7286bea245ff0
Matthias Dittrich <matthi.d@googlemail.com>
2015-09-20 21:43:25 +0200

    Bump version to 0.9.3-alpha2

--- src/SharedAssemblyInfo.fs
+++ src/SharedAssemblyInfo.fs
@@ -4,20 +4,10 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Yaaf.AdvancedBuilding")>]
 [<assembly: AssemblyProductAttribute("Yaaf.AdvancedBuilding")>]
 [<assembly: AssemblyCopyrightAttribute("Yaaf.AdvancedBuilding Copyright © Matthias Dittrich 2015")>]
-<<<<<<< Updated upstream
 [<assembly: AssemblyVersionAttribute("0.9.3")>]
 [<assembly: AssemblyFileVersionAttribute("0.9.3")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.9.3-alpha1")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.9.3-alpha2")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] Version = "0.9.3"
-=======
-[<assembly: AssemblyVersionAttribute("0.4.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.4.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.4.0")>]
-do ()
-
-module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.4.0"
->>>>>>> Stashed changes
