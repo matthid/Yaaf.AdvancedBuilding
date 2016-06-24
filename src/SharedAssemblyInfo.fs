870161910fccaaf8c3bf3ffed717b1172d76bd80
Matthias Dittrich <matthi.d@googlemail.com>
2016-06-24 17:59:04 +0200

    Bump version to 0.14.1

--- src/SharedAssemblyInfo.fs
+++ src/SharedAssemblyInfo.fs
@@ -4,11 +4,11 @@ open System.Reflection
 [<assembly: AssemblyCompanyAttribute("Yaaf.AdvancedBuilding")>]
 [<assembly: AssemblyProductAttribute("Yaaf.AdvancedBuilding")>]
 [<assembly: AssemblyCopyrightAttribute("Yaaf.AdvancedBuilding Copyright © Matthias Dittrich 2015")>]
-[<assembly: AssemblyVersionAttribute("0.14.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.14.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.14.0")>]
+[<assembly: AssemblyVersionAttribute("0.14.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.14.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.14.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.14.0"
-    let [<Literal>] InformationalVersion = "0.14.0"
+    let [<Literal>] Version = "0.14.1"
+    let [<Literal>] InformationalVersion = "0.14.1"
